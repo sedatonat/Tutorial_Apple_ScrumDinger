e9882d0b4546b303fd88bca3e8c3b67de056c9f8
Sedat Onat <sedatonat@outlook.com>
2022-10-03 21:26:02 +0300

    Initial Commit

---
+++ Tutorial_Apple_ScrumDinger/Tutorial_Apple_ScrumDingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tutorial_Apple_ScrumDingerApp.swift
+//  Tutorial_Apple_ScrumDinger
+//
+//  Created by Sedat Onat on 3.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct Tutorial_Apple_ScrumDingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
