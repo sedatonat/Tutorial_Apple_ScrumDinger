d7659400779b4219e84934077db03a7657d46d11
Sedat Onat <sedatonat@outlook.com>
2022-10-04 05:50:58 +0300

    Update MeetingView.swift

--- Tutorial_Apple_ScrumDinger/MeetingView.swift
+++ Tutorial_Apple_ScrumDinger/MeetingView.swift
@@ -9,11 +9,34 @@ import SwiftUI
 
 struct MeetingView: View {
     var body: some View {
-        ProgressView(value:5, total:15)
+        VStack {
+            ProgressView(value:5, total:15)
+            HStack{
+                VStack (alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack (alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.tophalf.filled")
+                }
+            }
+            Circle()
+                .strokeBorder(lineWidth:24)
+            HStack{
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button (action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+            }
+        }
+        .padding()
     }
 }
-
-//test
 
 struct MeetingView_Previews: PreviewProvider {
     static var previews: some View {
