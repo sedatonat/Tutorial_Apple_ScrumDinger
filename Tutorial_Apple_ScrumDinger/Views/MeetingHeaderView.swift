975531268ffc8f22f8c543ba77a3f6ecf09a490f
Sedat Onat <sedatonat@outlook.com>
2022-10-16 05:47:44 +0300

    Update
    "Managing state and life cycle / Section 3" ok
    Apple Tutorial 'inda atlanan bir SucrumDingerViewStyle dosyasindan dolayi hata veriyordu. ilgili View 'u bitmis dosyadan aldim

--- Tutorial_Apple_ScrumDinger/Views/MeetingHeaderView.swift
+++ Tutorial_Apple_ScrumDinger/Views/MeetingHeaderView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct MeetingHeaderView: View {
     let secondsElapsed: Int
     let secondsRemaining: Int
+    let theme: Theme
         
     private var totalSeconds: Int {
         secondsElapsed + secondsRemaining
@@ -29,6 +30,7 @@ struct MeetingHeaderView: View {
         
         VStack {
             ProgressView(value:progress)
+                .progressViewStyle(ScrumProgressViewStyle(theme: theme)) // Bu kisim Section 2 de atlanmis. #feedback2Apple
             HStack{
                 VStack (alignment: .leading) {
                     Text("Seconds Elapsed")
@@ -41,19 +43,21 @@ struct MeetingHeaderView: View {
                     Text("Seconds Remaining")
                         .font(.caption)
                     Label("\(secondsRemaining)", systemImage: "hourglass.tophalf.filled")
+                        .labelStyle(.trailingIcon) // ikonu saga atti
                 }
             }
         } // VStack eklenmeden evvel asagidakile gorunmuyordu. Layer lara dikkat etmek gerekli. bir layer digerini ezebiliyor. #learn
         .accessibilityElement(children: .ignore)
         .accessibilityLabel("Time remaining")
         .accessibilityValue("\(minutesRemaining) minutes")
+        .padding([.top, .horizontal])
         
     }
 }
 
 struct MeetingHeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        MeetingHeaderView(secondsElapsed: 60, secondsRemaining: 180)
+        MeetingHeaderView(secondsElapsed: 60, secondsRemaining: 180, theme: .bubblegum) // buradaki .theme kismi atlanmis Section 2-3 arasinda #feedback2Apple
             .previewLayout(.sizeThatFits)
     }
 }
