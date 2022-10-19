de6554e75acfe9811f0689064fc595296ac08bd5
Sedat Onat <sedatonat@outlook.com>
2022-10-19 22:08:45 +0300

    Update
    Modernizing asynchronous code / Section 2
    Finished

--- Tutorial_Apple_ScrumDinger/Data/ScrumStore.swift
+++ Tutorial_Apple_ScrumDinger/Data/ScrumStore.swift
@@ -23,7 +23,25 @@ class ScrumStore: ObservableObject {
         .appendingPathComponent("scrums.data")
     }
     
-    // Doayayi yukluyor
+    
+    static func load() async throws -> [DailyScrum] { // Bir parametreden sonra ayns yazmak bu parametrenin async oldugunu belirtir
+        try await withCheckedThrowingContinuation { continuation in
+            // Tutorial Note: "Calling withCheckedThrowingContinuation suspends the load function, then passes a continuation into a closure that you provide. A continuation is a value that represents the code after an awaited function." Anlamadim #learn
+            load {result in
+                switch result {
+                case .failure(let error): // hata olursa error yolla
+                    continuation.resume(throwing: error)
+                case .success(let scrums): // Hata olmazsa scrums calistir
+                    continuation.resume(returning: scrums)
+                    // Tutorial Note: "Add a switch statement to handle the result cases." #learn
+                }
+            }
+        }
+    }
+    
+    
+    
+    // Dosyayi yukluyor
     static func load(completion: @escaping (Result<[DailyScrum], Error>)->Void) {
         DispatchQueue.global(qos: .background).async {
             do { // Herhangi bir hata ile karsilasildiginda yapilacak olan islem icin kullaniliyor
